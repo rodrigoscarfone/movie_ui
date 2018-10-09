6391e48a1ce877033fbe7e2a973f0ca40f7ee7f1
Rodrigo <rodrigoscarfone@users.noreply.github.com>
2018-10-09 00:28:45 -0300

    packages include

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+
+import './App.scss';
 
 
 class App extends Component {
